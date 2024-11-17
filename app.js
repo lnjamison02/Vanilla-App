0c810c2f31507298383ae09bf725502f8f8a6986
lnjamison02 <lnjamison02@gmail.com>
2024-11-17 14:39:08 -0500

    Updates

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function handleSearchSubmit(event){
+function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("search-form-input");
-    console.log(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);

a5cdc4e73a83ca70b56561a6d81193505e71fb2c
lnjamison02 <lnjamison02@gmail.com>
2025-01-19 12:03:35 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -83,7 +83,7 @@ function handleSearchSubmit(event) {
 
   function getForecast(city){
     let apiKey = "b2a5adcct04b33178913oc335f405433";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
     axios(apiUrl).then(displayForecast);
   }
 
